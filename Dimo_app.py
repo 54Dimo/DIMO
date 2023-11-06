c52a10184789495eaca0f1a1227962afa77380a0
54Dimo <149690894+54Dimo@users.noreply.github.com>
2023-11-06 11:57:35 +0800

    Update Dimo_app.py

--- Dimo_app.py
+++ Dimo_app.py
@@ -271,6 +271,7 @@ def handle_postback(event):
                 Rmenu.contect2Us(event, user_id, line_bot_api)
         elif action=='stdQuestion':
             if value=='exerciseAndMeal':
+		print('res-exercise')
                 question.respond_exerciseAndMeal(event, user_id, line_bot_api)
             elif value=='resipeOffering':
                 question.respond_resipeOffering(event, user_id, line_bot_api)
