1793ef5cdd09e9aaea51c38c147841c0093b6d4b
54Dimo <149690894+54Dimo@users.noreply.github.com>
2023-11-08 17:18:17 +0800

    Update Dimo_viewRecord.py

--- Dimo_viewRecord.py
+++ Dimo_viewRecord.py
@@ -12,178 +12,242 @@ def item_create(index,block,data):
             "type": "box",
             "layout": "vertical",
             "contents": [
-                {
-                 "type": "box",
-            "layout": "horizontal",
-            "contents": [
               {
-                "type": "text",
-                "text": f"{data[index][4]}",
-                "weight": "bold",
-                "size": "md",
-                "wrap": True,
-                "flex": 7
-              },
-              {
-                "type": "text",
-                "text": f"份數: {data[index][10]}",
-                "size": "xxs",
-                "flex": 3,
-                "align": "start",
-                "gravity": "bottom",
-                "offsetTop": "xs"
-              }
-            ],
+                "type": "box",
+                "layout": "horizontal",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": f"{data[index][4]}",
+                    "weight": "bold",
+                    "size": "sm",
+                    "wrap": true,
+                    "flex": 7
+                  },
+                  {
+                    "type": "text",
+                    "text": f"{data[index][10]}份",
+                    "size": "xxs",
+                    "flex": 2,
+                    "align": "end",
+                    "gravity": "bottom",
+                    "offsetTop": "xs"
+                  }
+                ],
                 "height": "20px",
                 "flex": 2
-                },
-                {
+              },
+              {
                 "type": "separator",
                 "margin": "sm"
-                },
-                {
+              },
+              {
                 "type": "box",
                 "layout": "vertical",
                 "contents": [
-                    {
+                  {
                     "type": "box",
                     "layout": "vertical",
                     "contents": [
-                        {
-                        "type": "text",
-                        "text": "熱量",
-                        "size": "xs"
-                        },
-                        {
+                      {
+                        "type": "box",
+                        "layout": "horizontal",
+                        "contents": [
+                          {
+                            "type": "text",
+                            "text": "熱量",
+                            "size": "xs",
+                            "flex": 6
+                          },
+                          {
+                            "type": "text",
+                            "text": f"{(data[index][6]*data[index][10])}大卡",
+                            "size": "xxs",
+                            "align": "start",
+                            "gravity": "bottom",
+                            "offsetTop": "xs",
+                            "flex": 3
+                          }
+                        ]
+                      },
+                      {
                         "type": "box",
                         "layout": "vertical",
                         "contents": [
-                            {
+                          {
                             "type": "filler"
-                            }
+                          }
                         ],
                         "height": "6px",
-                        "width": f"{(data[index][6]*data[index][10])/10}%",
-                        "backgroundColor": "#000000"
-                        }
+    					"width": f"{(data[index][6]*data[index][10])/5}%",
+                        "backgroundColor": "#4b77a8"
+                      }
                     ],
                     "height": "22px"
-                    },
-                    {
+                  },
+                  {
                     "type": "box",
                     "layout": "vertical",
                     "contents": [
-                        {
-                        "type": "text",
-                        "text": "蛋白質",
-                        "size": "xs"
-                        },
-                        {
+                      {
+                        "type": "box",
+                        "layout": "horizontal",
+                        "contents": [
+                          {
+                            "type": "text",
+                            "text": "蛋白質",
+                            "size": "xs",
+                            "flex": 6
+                          },
+                          {
+                            "type": "text",
+                            "text": f"{(data[index][7]*data[index][10])}公克",
+                            "size": "xxs",
+                            "align": "start",
+                            "gravity": "bottom",
+                            "offsetTop": "xs",
+                            "flex": 3
+                          }
+                        ]
+                      },
+                      {
                         "type": "box",
                         "layout": "vertical",
                         "contents": [
-                            {
+                          {
                             "type": "filler"
-                            }
+                          }
                         ],
                         "height": "6px",
-                        "width": f"{(data[index][7]*data[index][10])/10}%",
-                        "backgroundColor": "#000000"
-                        }
+    					"width": f"{(data[index][7]*data[index][10])/2}%",
+                        "backgroundColor": "#4aa04a"
+                      }
                     ],
                     "height": "22px"
-                    },
-                    {
+                  },
+                  {
                     "type": "box",
                     "layout": "vertical",
                     "contents": [
-                        {
-                        "type": "text",
-                        "text": "脂肪",
-                        "size": "xs"
-                        },
-                        {
+                      {
+                        "type": "box",
+                        "layout": "horizontal",
+                        "contents": [
+                          {
+                            "type": "text",
+                            "text": "脂肪",
+                            "size": "xs",
+                            "flex": 6
+                          },
+                          {
+                            "type": "text",
+                            "text": f"{(data[index][8]*data[index][10])}公克",
+                            "size": "xxs",
+                            "align": "start",
+                            "gravity": "bottom",
+                            "offsetTop": "xs",
+                            "flex": 3
+                          }
+                        ]
+                      },
+                      {
                         "type": "box",
                         "layout": "vertical",
                         "contents": [
-                            {
+                          {
                             "type": "filler"
-                            }
+                          }
                         ],
                         "height": "6px",
-                        "width": f"{(data[index][8]*data[index][10])/10}%",
-                        "backgroundColor": "#000000"
-                        }
+    					"width": f"{(data[index][8]*data[index][10])/2}%",
+                        "backgroundColor": "#cf5954"
+                      }
                     ],
                     "height": "22px"
-                    },
-                    {
+                  },
+                  {
                     "type": "box",
                     "layout": "vertical",
                     "contents": [
-                        {
-                        "type": "text",
-                        "text": "碳水化合物",
-                        "size": "xs"
-                        },
-                        {
+                      {
+                        "type": "box",
+                        "layout": "horizontal",
+                        "contents": [
+                          {
+                            "type": "text",
+                            "text": "碳水化合物",
+                            "size": "xs",
+                            "flex": 6
+                          },
+                          {
+                            "type": "text",
+                            "text": f"{(data[index][9]*data[index][10])}公克",
+                            "size": "xxs",
+                            "align": "start",
+                            "gravity": "bottom",
+                            "offsetTop": "xs",
+                            "flex": 3
+                          }
+                        ]
+                      },
+                      {
                         "type": "box",
                         "layout": "vertical",
                         "contents": [
-                            {
+                          {
                             "type": "filler"
-                            }
+                          }
                         ],
                         "height": "6px",
-                        "width": f"{(data[index][9]*data[index][10])/10}%",
-                        "backgroundColor": "#000000"
-                        }
+    					"width": f"{(data[index][9]*data[index][10])/2}%",
+                        "backgroundColor": "#e59d32"
+                      }
                     ],
                     "height": "22px"
-                    }
+                  }
                 ],
                 "margin": "sm",
                 "spacing": "xs",
                 "flex": 6
-                },
-                {
+              },
+              {
                 "type": "separator",
                 "margin": "md"
-                },
-                {
+              },
+              {
                 "type": "box",
                 "layout": "horizontal",
                 "contents": [
-                    {
+                  {
                     "type": "button",
                     "action": {
-                        "type": "postback",
-                        "label": "修改",
-                        "data": "udRecord^"+str(data[index][0])+":"+data[index][4]+":"+str(data[index][2])
+                      "type": "postback",
+                      "label": "修改",
+                      "data": "udRecord^"+str(data[index][0])+":"+data[index][4]+":"+str(data[index][2])
                     },
                     "flex": 5,
                     "height": "sm"
-                    },
-                    {
+                  },
+                  {
                     "type": "separator"
-                    },
-                    {
+                  },
+                  {
                     "type": "button",
                     "action": {
-                        "type": "postback",
-                        "label": "刪除",
-                        "data": "dlRecord^"+str(data[index][0])+":"+data[index][4]+":"+str(data[index][2])
+                      "type": "postback",
+                      "label": "刪除",
+                      "data": "dlRecord^"+str(data[index][0])+":"+data[index][4]+":"+str(data[index][2])
                     },
                     "flex": 5,
                     "height": "sm"
-                    }
+                  }
                 ],
                 "spacing": "xs",
                 "flex": 2
-                },
-                {
-                "type": "separator",
-                }
+              },
+              {
+                "type": "separator"
+              }
             ],
             "height": "180px",
             "paddingAll": "8px"
